5af235b409854c8a84461b8e8119958e40c218e1
Marco Civello <marco.civello@gmail.com>
2021-05-23 10:42:36 +0200

    feat: button functionality
    pressing the button the items are shown to the screen

--- App.js
+++ App.js
@@ -1,41 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
   const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  const HandleValue = (text) => {
+  const todosHandler = (text) => {
     setInputValue(text);
   };
 
+  const addTodosHandler = () => {
+    setTodos((todos) => [
+      ...todos,
+      { id: Math.random().toString(), value: inputValue },
+    ]);
+  };
+
   return (
-    <View style={styles.Container}>
+    <View style={styles.Screen}>
       <Text style={styles.Title}>MY TODO APP</Text>
       <View style={styles.InputContainer}>
         <TextInput
-          onChangeText={HandleValue}
+          onChangeText={todosHandler}
           value={inputValue}
           style={styles.Input}
           placeholder='Todo List'
         />
-        <Button title='Enter' onPress={() => {}} />
+        <Button title='Add' onPress={addTodosHandler} />
       </View>
-      <View>
-        <Text></Text>
-      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={todos}
+        renderItem={(itemData) => (
+          <View style={styles.Todos}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}></FlatList>
       <StatusBar style='auto' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  Container: {
-    borderWidth: 1,
-    backgroundColor: '#fff',
-    flex: 1,
+  Screen: {
     padding: 50,
-    alignItems: 'center',
   },
   Title: {
     marginTop: 30,
@@ -44,15 +60,24 @@ const styles = StyleSheet.create({
   },
   InputContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
+    justifyContent: 'space-between',
     marginTop: 20,
   },
   Input: {
     width: '80%',
     borderWidth: 1,
     borderRadius: 5,
-    paddingLeft: 5,
-    paddingVertical: 7,
+    paddingHorizontal: 5,
+    paddingVertical: 8,
     marginRight: 10,
+    borderWidth: 1,
+    backgroundColor: '#fff',
+  },
+  Todos: {
+    padding: 10,
+    marginTop: 20,
+    backgroundColor: '#ccc',
+    borderColor: '#333',
+    borderWidth: 1,
   },
 });
